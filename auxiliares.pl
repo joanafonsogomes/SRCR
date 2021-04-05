99f48f6ff6e482c4a6747344e38656a05a8d06a1
joanafonsogomes <afonnso.gommes@gmail.com>
2021-04-05 13:31:55 +0100

    comment bug in auxiliares file

--- auxiliares.pl
+++ auxiliares.pl
@@ -1,32 +1,9 @@
 % Auxiliares
-
+/*
 % Insere conhecimento na base de conhecimento
 novoConhecimento(T) :- solucoes(I, +T::I, Linv),
                         insercao(T),
-                        teste(Linv).
-
-/* % Verificar se o gérnero é válido
-generoValido('M').
-generoValido('F').
-
-% Verificar se a data é válida : Data -> {V,F}
-data(Dia,Mes,Ano) :- anoValido(Ano), mesValido(Mes), diaValido(Dia,Mes).
-
-% Verificar se o ano é válido : Ano -> {V,F}
-anoValido(Ano) :- natural(Ano), Ano > 1890 , Ano < 2020.
-
-%  : Mes -> {V,F}
-mesValido(Mes) :- natural(Mes), Mes < 13.
-
-% Verificar se o dia é válido: Dia, Mes, Ano -> {V,F}
-diaValido(Dia,Mes) :- pertence(Mes,[1,3,5,7,8,10,12]),
-                    natural(Dia),
-                    Dia < 32.
-diaValido(Dia,Mes) :- pertence(Mes,[4,6,9,11]),
-                        natural(Dia),
-                        Dia < 31.
-
-diaValido(Dia,2) :- natural(Dia), Dia < 29. % não estamos a albergar anos bissextos...*/
+                        teste(Linv).*/
 
 % Verifica se a segunda data e anterior a primeira
 comparaDatas(datime(AH,MH,DH,,,_),data(AP,MP,DP)) :-
