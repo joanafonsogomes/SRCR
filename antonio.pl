aca2e3da25001b8940b004bc0d2e0c8975674a68
António Santos <antoniomsantos99@gmail.com>
2021-04-06 16:13:21 +0100

    Nome do utente

--- antonio.pl
+++ antonio.pl
@@ -7,3 +7,6 @@ vacinados1Dose(R) :- solucoes(UId,vacinacao(A,UId,B,C,1),R).
 vacinados2Doses(R) :- solucoes(UId,vacinacao(A,UId,B,C,1),L1),
                 solucoes(UId,vacinacao(A,UId,B,C,2),L2),
                 intersecao(L1,L2,R).
+
+% by request (Pode dar jeito no futuro)
+nomeDoUtente(UId,X) :- utente(UId,X,A,B,C,D,E,F,G,H,I).
