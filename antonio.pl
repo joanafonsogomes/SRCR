a417edb3cd746ee3714f4aa3bb6950c0c32a840a
António Santos <antoniomsantos99@gmail.com>
2021-04-06 16:46:32 +0100

    Não Vacinados feito (ty Paulo!)

--- antonio.pl
+++ antonio.pl
@@ -10,3 +10,14 @@ vacinados2Doses(R) :- solucoes(UId,vacinacao(A,UId,B,C,1),L1),
 
 % by request (Pode dar jeito no futuro)
 nomeDoUtente(UId,X) :- utente(UId,X,A,B,C,D,E,F,G,H,I).
+
+% Averigua se elemento esta vacinado
+checkVacinado(UId,X) :- vacinacao(A,UId,B,C,D).
+
+% Elimina os utentes vacinados
+naoVacinados(R) :- listaIdUtentes(X),
+                   naoVacinadosAux(X,[],R).
+
+naoVacinadosAux([],Acc,Acc).
+naoVacinadosAux([H|T],Acc,R) :- checkVacinado(H,Acc), naoVacinadosAux(T,Acc,R).
+naoVacinadosAux([H|T],Acc,R) :- naoVacinadosAux(T,[H|Acc],R).
