47054c407c7fa18755830483b2ae881fc4112883
António Santos <antoniomsantos99@gmail.com>
2021-04-06 23:15:00 +0100

    Apenas primeira dose vacinados

--- antonio.pl
+++ antonio.pl
@@ -12,12 +12,20 @@ vacinados2Doses(R) :- solucoes(UId,vacinacao(A,UId,B,C,1),L1),
 nomeDoUtente(UId,X) :- utente(UId,X,A,B,C,D,E,F,G,H,I).
 
 % Averigua se elemento esta vacinado
-checkVacinado(UId,X) :- vacinacao(A,UId,B,C,D).
+checkVacinado(UId,X,F) :- vacinacao(A,UId,B,C,F).
 
 % Elimina os utentes vacinados
 naoVacinados(R) :- listaIdUtentes(X),
                    naoVacinadosAux(X,[],R).
 
 naoVacinadosAux([],Acc,Acc).
-naoVacinadosAux([H|T],Acc,R) :- checkVacinado(H,Acc), naoVacinadosAux(T,Acc,R).
+naoVacinadosAux([H|T],Acc,R) :- checkVacinado(H,Acc,1),checkVacinado(H,Acc,2), naoVacinadosAux(T,Acc,R).
 naoVacinadosAux([H|T],Acc,R) :- naoVacinadosAux(T,[H|Acc],R).
+
+% Só primeira dose 
+apenasPrimeiraDoseVacinados(R) :- listaIdUtentes(X),
+                            apenasPrimeiraDoseVacinadosAux(X,[],R).
+
+apenasPrimeiraDoseVacinadosAux([],Acc,Acc).
+apenasPrimeiraDoseVacinadosAux([H|T],Acc,R) :- (checkVacinado(H,Acc,1),\+checkVacinado(H,Acc,2)), apenasPrimeiraDoseVacinadosAux(T,[H|Acc],R).
+apenasPrimeiraDoseVacinadosAux([H|T],Acc,R) :- apenasPrimeiraDoseVacinadosAux(T,Acc,R).
