c5ccc6a6e9b464eaec6b9a472e367d440b752f1e
joanafonsogomes <afonnso.gommes@gmail.com>
2021-05-01 19:14:14 +0100

    updt conhecimento perf

--- TP2/conhecimento.pl
+++ TP2/conhecimento.pl
@@ -4,7 +4,10 @@
 
 % ----- Conhecimento Perfeito -----
 
-% Conhecimento Perfeito Positivo
+% - Conhecimento Perfeito Positivo -
+
+% utente
+% #IdUtente, Nr Seguranca Social, Nome, Genero, Data Nascimento, Email, Telefone, Morada, Profissao, [Doencas Cronicas], #IdCentroSaude
 utente('U0','Romulo Mota','22337840788','M','1936-04-22','RomuloMota@outlook.com',962935227,'Rua Francisco Augusto Alvim 4700-004 Braga','Biologo',[],'C1').
 utente('U1','Goncalo Melo','65105592957','M','1931-09-18','GoncaloMelo1991@hotmail.com',938823950,'Travessa 1 de Maio 4700-008 Braga','Geriatra',[],'C2').
 utente('U2','Bianca Moura','84874182284','F','1958-10-30','BiancaMoura244@gmail.com',921591637,'Rua de Sao Rosendo ( Bispo de Dume ) Dume 4700-008 Braga','Personal trainer',[],'C2').
@@ -22,6 +25,8 @@ utente('U17','Dinarda Matias','29224473940','F','1974-10-09','DinardaMatias1974@
 utente('U18','Marcilene Goncalves','77919813012','M','1989-12-04','MarcileneGoncalves305@hotmail.com',924527670,'Rua do Carvalhal Dume 4700-005 Braga','Perito criminal',['Doenca coronaria'],'C1').
 utente('U19','Milena Vicente','39280061650','F','2004-06-04','MilenaVicente@gmail.com',969590100,'Travessa General Humberto Delgado 4760-001 Vila Nova de Famalicao','Jovem Aprendiz',[],'C2').
 
+% staff
+% #IdStaff, IdCentroSaude, Nome, Email
 staff('S0','Leonardina Anjos','12287559265','LeonardinaAnjos49@outlook.com','C2').
 staff('S1','Fernando Guerreiro','28567061757','FernandoGuerreiro@outlook.com','C2').
 staff('S2','Idelia Paiva','61816783608','IdeliaPaiva@hotmail.com','C1').
@@ -32,10 +37,14 @@ staff('S7','Juliao Abreu','54101439180','JuliaoAbreu@gmail.com','C4').
 staff('S9','Tatiana Faria','59173595792','TatianaFaria1958@outlook.com','C4').
 staff('S10','Juliano Campos','62975266565','JulianoCampos1991@hotmail.com','C2').
 
+% centro
+% #IdCentroSaude, Nome, Morada, Telefone, Email 
 centro('C1','Centro de saude de Braga','Rua Paulo Vi 4700-004 Braga','219229219','suporte@debraga.com').
 centro('C2','Centro de saude do Caranda','Rua de Goa 4800-004 Guimaraes','246512282','suporte@docaranda.com').
 centro('C4','Centro de saude Dr.Paulo Novais','Rua Francisco Ribeiro de Castro 4800-004 Guimaraes','219282688','suporte@drpaulonovais.com').
 
+% vacinacao 
+%  #IdStaff, #IdUtente, Data, Vacina, Toma
 vacinacao('S10','U1','2021-06-08','Moderna',1).
 vacinacao('S10','U1','2021-06-30','Moderna',2).
 vacinacao('S15','U2','2021-07-30','Pfizer',1).
@@ -60,7 +69,13 @@ vacinacao('S5','U14','2021-06-30','Sputnik V',1).
 vacinacao('S5','U14','2021-07-23','Sputnik V',2).
 vacinacao('S3','U17','2021-07-27','Pfizer',1).
 
-% Conhecimento Perfeito Negativo 
+% fase
+% #IdFase, DataInicio, DataFim
+fase('F1','2020-12-01','2021-03-31').
+fase('F2','2021-04-01','2021-08-31').
+fase('F3','2021-09-01','9999-12-31').
+
+% - Conhecimento Perfeito Negativo -
 -utente('U16','Gino Campos','21515788464','M','2004-03-23','GinoCampos@gmail.com',920820869,'Lugar da Ordem 4700-008 Braga','Piloto de aviao',[],'C4').
 -utente('U21','Rodrigo Matos','90131842525','M','1996-06-04','RodrigoMatos@gmail.com',930344386,'Largo da Confeiteira Dume 4700-005 Braga','Cientista de dados',[],'C1').
 
@@ -74,20 +89,22 @@ vacinacao('S3','U17','2021-07-27','Pfizer',1).
 
 % ----- Conhecimento Imperfeito -----
 
-% Conhecimento Imperfeito Incerto
+% - Conhecimento Imperfeito Incerto -
 utente('U13','Adelia Amaral','41527730111','F','1944-09-26',email_desconhecido,910419566,'Rua Doutor Joao Afonso Almeida Azurem 4800-004 Guimaraes','Bioquimico',[],'C2').
 excecao(utente(Id, N, G, DN, E, T, M, P, DC, IdCentro)) :- utente(Id, N, G, DN, email_desconhecido, T, M, P, DC, IdCentro)
 
-% Conhecimento Imperfeito Impreciso
+% - Conhecimento Imperfeito Impreciso -
 excecao(utente('U12','Biana Andrade','03294496759','F','1982-05-02','BianaAndrade@hotmail.com',932477556,'Rua de Sao Martinho Dume 4700-008 Braga','Padeiro',[],'C1')).
 excecao(utente('U12','Biana Andrade','03294496759','F','1982-05-02','BianaAndrade@hotmail.com',932477556,'Rua do Monte 4700-009 Braga','Padeiro',[],'C1')).
 
 excecao(utente('U6','Zelia Abreu','94334540382','F','1995-06-19','ZeliaAbreu@hotmail.com',966727220,'Rua do Brasil 4775-001 Cambeses','Eletricista',[],'C2')).
 excecao(utente('U6','Zelia Abreu','94334540382','F','1995-06-19','ZeliaAbreu@hotmail.com',916779510,'Rua do Brasil 4775-001 Cambeses','Eletricista',[],'C2')).
 
-% Conhecimento Imperfeito Interdito
+% - Conhecimento Imperfeito Interdito -
 utente('U20','Delmiro Lopes','36606379205','M','1956-05-23',email_desconhecido,927819307,'Rua Monte de Baixo 4705-001 Arentim','Cerimonialista',[],'C1').
 excecao(utente(Id,N,NSS,G,Dn,E,T,M,P,D,Cs)) :- utente(Id,N,NSS,G,Dn,email_impossivel,T,M,P,D,Cs).
 nulointerdito(email_impossivel).
 +utente(Id,N,NSS,G,Dn,E,T,M,P,D,Cs) ::  (solucoes(Id,N,NSS,G,Dn,E,T,M,P,D,Cs), utente('U20','Delmiro Lopes','36606379205','M','1956-05-23',email_impossivel,927819307,'Rua Monte de Baixo 4705-001 Arentim','Cerimonialista',[],'C1'), nao(nulointerdito(email_impossivel))), R), 
                                         comprimento(R,0)).
+
+
--- TP2/evolucao.pl
+++ TP2/evolucao.pl
@@ -2,6 +2,8 @@
 % Evolução de Conhecimento
 % ---------------------------------
 
+% ----- Conhecimento Perfeito -----
+
 evolucaoC(T) :- solucoes(I, +T::I, L),
                 insercao(T),
                 testaPredicados(L)
@@ -14,14 +16,14 @@ insercao(Q) :- retract(Q), !, fail.
 testaPredicados([]).
 testaPredicados([I|L]) :- I, testaPredicados(L).
 
-% ----- Conhecimento Perfeito -----
-
 % Conhecimento Perfeito Positivo
-
-% ...
 
 % Conhecimento Perfeito Negativo
 
 evolucaoC(T,neg) :- solucoes(I, +(-T)::I, L),
                 insercao(-T),
                 testaPredicados(L)
+
+% ----- Conhecimento Imperfeito -----
+
+% Conhecimento Imperfeito Incerto
