0128fbf00c9eab86152b9b4f0a895c828c6318d1
joanafonsogomes <afonnso.gommes@gmail.com>
2021-05-01 18:43:39 +0100

    evolucao update

--- TP2/evolucao.pl
+++ TP2/evolucao.pl
@@ -1,3 +1,19 @@
 % ---------------------------------
 % Evolução de Conhecimento
 % ---------------------------------
+
+evolucaoC(T) :- solucoes(I, +T::I, L),
+                insercao(T),
+                testaPredicados(L)
+
+solucoes(T,Q,S) :- findall(T,Q,S).
+
+insercao(Q) :- assert(Q).
+insercao(Q) :- retract(Q), !, fail.
+
+testaPredicados([]).
+testaPredicados([I|L]) :- I, testaPredicados(L).
+
+% ----- Conhecimento Perfeito -----
+
+% Conhecimento Perfeito Positivo
