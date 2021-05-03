7f4c8057d70676a81263c6ef7007b246a43b1287
joanafonsogomes <afonnso.gommes@gmail.com>
2021-05-03 13:42:08 +0100

    Inserir Conhecimento Imperfeito Impreciso START

--- TP2/evolucao.pl
+++ TP2/evolucao.pl
@@ -16,6 +16,8 @@ insercao(Q) :- retract(Q), !, fail.
 testaPredicados([]).
 testaPredicados([I|L]) :- I, testaPredicados(L).
 
+% ----------------------------------------------
+
 % Conhecimento Perfeito Positivo
 
 % UTENTE: Idutente, Nº Segurança_Social, Nome, Genero, Data_Nasc, Email, Telefone, Morada, Profissão, [Doenças_Crónicas], #CentroSaúde
@@ -30,12 +32,14 @@ novoCentro(Id,N,M,Tlf,E) :- evolucaoC(centro(Id,N,M,Tlf,E)).
 % VACINAÇÕES: IdStaff, IdUtente, Data, Vacina, Toma
 novaVacinacao(IdS,IdU,D,V,T) :- evolucaoC(vacinacao(IdS,IdU,D,V,T)).
 
+% ----------------------------------------------
 
 % Conhecimento Perfeito Negativo
 
+% Inserir Conhecimento Perfeito Negativo
 evolucaoC(T,conheNeg) :- solucoes(I, +(-T)::I, L),
-                insercao(-T),
-                testaPredicados(L).
+                        insercao(-T),
+                        testaPredicados(L).
 
 
 % ----- Evolucao do Conhecimento Imperfeito -----
@@ -45,30 +49,42 @@ evolucaoC(T,conheNeg) :- solucoes(I, +(-T)::I, L),
 % UTENTE
 
 % Inserir Conhecimento Perfeito Incerto para utente com email desconhecido
-evolucaoC(utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro), utente, conheImpInc, email) :-
-    evolucaoC(utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro)),
-    insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro))).
+evolucaoC(utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro), utente, conheImpInc, email) :- evolucaoC(utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro)),
+                                                                                                    insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- 
+                                                                                                        utente(Id,N,Nu,G,DN,email_desconhecido,T,M,P,DC,IdCentro))).
 
 % Inserir Conhecimento Perfeito Incerto para utente com telefone desconhecido
-evolucaoC(utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro), utente, conheImpInc, tlf) :-
-    evolucaoC(utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro)),
-    insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro))).
+evolucaoC(utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro), utente, conheImpInc, tlf) :- evolucaoC(utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro)),
+                                                                                                insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- 
+                                                                                                    utente(Id,N,Nu,G,DN,E,tlf_desconhecido,M,P,DC,IdCentro))).
 
 % Inserir Conhecimento Perfeito Incerto para utente com morada desconhecida
-evolucaoC(utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro), utente, conheImpInc, morada) :-
-    evolucaoC(utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro)),
-    insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro))).
+evolucaoC(utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro), utente, conheImpInc, morada) :- evolucaoC(utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro)),
+                                                                                        insercao((excecao(utente(Id,N,Nu,G,DN,E,T,M,P,DC,IdCentro)) :- 
+                                                                                            utente(Id,N,Nu,G,DN,E,T,morada_desconhecida,P,DC,IdCentro))).
 
 % STAFF
 
 % Inserir Conhecimento Perfeito Incerto para membro do staff com email desconhecido
-evolucaoC(staff(Id,IdC,N,email_desconhecido), staff, conheImpInc, email) :-
-    evolucaoC(staff(Id,IdC,N,email_desconhecido)),
-    insercao((excecao(staff(Id,IdC,N,E)) :- staff(Id,IdC,N,email_desconhecido))).
+evolucaoC(staff(Id,IdC,N,email_desconhecido), staff, conheImpInc, email) :- evolucaoC(staff(Id,IdC,N,email_desconhecido)),
+                                                                        insercao((excecao(staff(Id,IdC,N,E)) :- 
+                                                                            staff(Id,IdC,N,email_desconhecido))).
 
-
-
+% ----------------------------------------------
 
 % Evolucao do Conhecimento Imperfeito Impreciso
+
+% UTENTE
+
+% Inserir Conhecimento Perfeito Incerto para utente com email desconhecido
+
+% Inserir Conhecimento Imperfeito Impreciso 
+evolucaoC(T, conheImpImp) :- solucoes(I, +(excecao(T))::I, Lint),
+                            insercao(excecao(T)),
+                            testaPredicados(Lint).
+
+% STAFF
+
+% ----------------------------------------------
 
 % Evolucao do Conhecimento Imperfeito Interdito
